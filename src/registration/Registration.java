97edc9e12879ba5668fd1108a9d19b66e69dc586
DESKTOP-HQLQOR0\귀욤디욤 <wws17962@gmail.com>
2018-03-16 17:16:09 +0900

    개설교과목정보에서 띄어쓰기 수정

--- src/registration/Registration.java
+++ src/registration/Registration.java
@@ -3,32 +3,39 @@ package registration;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 
 public class Registration {
-
+	static String[] CourseList = new String[10];
+	
 	public static void readFile() throws IOException {
+		
+		int i=0;
 		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\귀욤디욤\\Documents\\registration-program\\src\\registration\\courses offered"));
 		try {
 			while(true) {
 				String line = br.readLine();
 				if (line==null) break;
 				System.out.println(line);
+				StringTokenizer tk= new StringTokenizer(line, " ");
+				String token;
+				token = tk.nextToken();
+				CourseList[i]=token;
+				i++;
 			}
 		}catch (IOException e){
 			e.printStackTrace();
 		}
 		finally {
-
 			br.close();}
 	}	
 	
 	public static void main(String[] args) {
 		String[] ApplicationList = new String[15] ;
 		Scanner sc = new Scanner(System.in);
-
+		
 		System.out.println("<2018 1학기 수강신청>\n");
 		System.out.println("<개설교과목 정보>");
 		try {
@@ -36,6 +43,7 @@ public class Registration {
 		}catch(Exception e) {
 			System.out.println("error occurred");
 		}
+		System.out.println(CourseList.length);
 		
 		System.out.println("\n수강신청할 과목을 입력하시고 모든 과목 입력이 끝나면 'done' 를 입력하여 결과를 확인하시오");
 		System.out.println("_________________________________________________________\n");
